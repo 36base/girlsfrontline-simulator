56b18087f4f9654d7f78ffa67beec7975d365f5c
CirnoV <cirnov59@gmail.com>
2017-11-28 09:19:12 +0900

    링크 보호 구현

--- src/index.spec.js
+++ src/index.spec.js
@@ -3,7 +3,8 @@ import SimulateSlot from '../data/NTW99.json'
 import SimulateEnemy from '../data/Dreamer.json'
 
 describe('`new Simulator()`', () => {
-  const simulate = new Simulator()
+  // const simulate = new Simulator()
+  const simulate = new Simulator({realMode: true})
   simulate.init(SimulateSlot, SimulateEnemy)
   test('Battle test', () => {
     simulate.start()
